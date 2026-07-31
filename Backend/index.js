11612eca8323276d1decb4682760932023f9bd8a
Aman Shrivastav <ak7217300@gmail.com>
2026-07-31 13:33:46 +0530

    Updated Code

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ import path from 'path';
 
 dotenv.config();
 const app=express();
-const __dirname=path.resolve();
+const _dirname=path.resolve();
 app.use(express.json());
 app.use(fileUpload());
 app.use(cors());
@@ -19,9 +19,9 @@ app.use("/img",express.static("uploads"));
  app.use('/api',router);
  app.use('/api',adminRoute);
 
- app.use(express.static(path.join(__dirname, "/frontend/dist")));
+ app.use(express.static(path.join(_dirname, "/frontend/dist")));
  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
   });
 app.listen(PORT,()=>{
     console.log("Server running on port "+PORT ); 
